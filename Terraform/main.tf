ee4a136977f1d18fbb29d65d410d442939ef354a
guillemangulo <guillem.angulo9@gmail.com>
2025-11-02 11:39:16 +0000

    Full automation v1

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,16 +2,43 @@ provider "aws" {
 	region = "eu-west-1"
 }
 
-#I use the one I created manually to test
-data "aws_security_group" "CASecGroup" {
-	id	= "sg-0a2c7557221084a0b"
+resource "aws_security_group" "CA-SG" {
+  name        = "CA-SG"
+  description = "Allow SSH (22) and HTTP (80)"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+	# full open, this just for this project, in real environment wouldn't be like that
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+	# same for http
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "CA-SG"
+  }
 }
 
 resource "aws_instance" "CA-1-Instance" {
 	ami 			= "ami-033a3fad07a25c231"
 	instance_type 		= "t3.micro"
 	key_name		= "CAKey"
-	vpc_security_group_ids	= [data.aws_security_group.CASecGroup.id]
+	vpc_security_group_ids	= [aws_security_group.CA-SG.id]
 
 
 	tags = {
@@ -19,4 +46,8 @@ resource "aws_instance" "CA-1-Instance" {
 	}
 }
 
+output "instance_ip" {
+  description = "Public IP from EC2 instance."
+  value       = aws_instance.CA-1-Instance.public_ip
+}
 
